52572a86ff5c448a7fd59ba48395d89a034a83e7
GitHub Action <action@github.com>
2025-04-04 10:21:42 +0000

    Update tic-tac-term to 0.2.0

--- Formula/tic-tac-term.rb
+++ Formula/tic-tac-term.rb
@@ -1,9 +1,9 @@
 class TicTacTerm < Formula
   desc "Tic Tac Toe game for the terminal"
   homepage "https://github.com/finafisken/tic-tac-term"
-  url "https://github.com/finafisken/tic-tac-term/releases/download/v0.1.3/tic-tac-term-macos"
-  sha256 "a52af060f8ada7ef5ff1318c5f828007a48f8bc9df52b54a30424e012ab8c4ca" # Get this with `shasum -a 256 your_tarball.tar.gz`
-  version "0.1.3"
+  url "https://github.com/finafisken/tic-tac-term/releases/download/v0.2.0/tic-tac-term-macos"
+  sha256 "b25892f00fdd652e97a66af08e1fd9826eb30b5d1437211e74d5e8621dddced1" # Get this with `shasum -a 256 your_tarball.tar.gz`
+  version "0.2.0"
   
   def install
     bin.install "tic-tac-term-macos" => "tic-tac-term"
